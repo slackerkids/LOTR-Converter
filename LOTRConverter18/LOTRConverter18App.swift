77d8f55f29b7a3a0505eb9f1e6e2be954640ca25
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-10-28 13:58:43 +0500

    Initial Commit

---
+++ LOTRConverter18/LOTRConverter18App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter18App.swift
+//  LOTRConverter18
+//
+//  Created by Sagidanov Bakhtiyar on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
