a533b46ed32d8c092243c8fa4f339c04386ed30a
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-11-05 18:11:03 +0500

    Selection Currency

--- LOTRConverter18/SelectCurrency.swift
+++ LOTRConverter18/SelectCurrency.swift
@@ -24,7 +24,11 @@ struct SelectCurrency: View {
                     .fontWeight(.bold)
                 
                 // Currency icons
-                CurrencyIcon(currencyImage: .copperpenny, currencyName: "Copper Penny")
+                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+                    ForEach(0..<5) { _ in
+                        
+                    }
+                }
                 
                 // Text
                 Text("Select the currency you would like to convert to:")
