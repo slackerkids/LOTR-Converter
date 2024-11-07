ce4ea1e11d05abb3deab7dc43edd8e1f47205b8c
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-11-07 10:08:28 +0500

    Model done

--- LOTRConverter18/Currency.swift
+++ LOTRConverter18/Currency.swift
@@ -5,10 +5,42 @@
 //  Created by Sagidanov Bakhtiyar on 06.11.2024.
 //
 
+import SwiftUI
+
 enum Currency: Double {
-    case copperPenny
-    case silverPenny
-    case silverPiece
-    case goldPenny
-    case goldPiece
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+    
+    var image: ImageResource {
+        switch self {
+        case .copperPenny:
+                .copperpenny
+        case .silverPenny:
+                .silverpenny
+        case .silverPiece:
+                .silverpiece
+        case .goldPenny:
+                .goldpenny
+        case .goldPiece:
+                .goldpiece
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
+    }
 }
